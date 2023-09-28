62d794ba1d72633121394c1252e1658b3ba6b16c
uagaviria <uagaviria@gmail.com>
2023-09-28 16:42:50 -0500

    ignore variables.h

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,10 @@ using namespace std;
 #include <MHZ19.h>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
+#include "variables.h"
 
-#define SENSOR_ID "v80_aprendiedo"
 
-#define FIXED_LAT "6.286984"
-#define FIXED_LON "-75.508214"
+
 
 //#define DEBUGGING
 
@@ -41,9 +40,7 @@ using namespace std;
 void conexion();
 int counter = 0;
 
-//WiFi
-//const char *wifi_ssid = "Daniel";
-//const char *wifi_password = "24752475";
+
 
 WiFiClient espclient;
 PubSubClient client(espclient);
@@ -201,19 +198,7 @@ void createDataFrame(){
 // MQTT Broker
 
 
-const char *mqtt_broker = "54.193.43.191";// broker address
-const char *topic = "datos/sensor"; // define topic 
-const char *temperatura = "datos/temperatura"; // define topic 
-const char *humedad = "datos/humedad"; // define topic 
-const char *aire = "datos/aire"; // define topic 
-const char *sonido = "datos/sonido"; // define topic 
-const char *co2 = "datos/co2"; // define topic 
-const char *estado_on = "datos/on"; // define topic 
-const char *estado_off = "datos/off"; // define topic 
 
-const char *mqtt_username = ""; // username for authentication
-const char *mqtt_password = "";// password for authentication
-const int mqtt_port = 1883;// port of MQTT over TCP
 
 void sendDataFrame(){
   createDataFrame();
